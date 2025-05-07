191e6fcc861eb45593a1877492cdb24569256337
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-05-07 11:43:21 +0200

    Realize EMA initialization and finalization in measure utility.

--- utils/measure/measure.c
+++ utils/measure/measure.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#include <EMA.h>
+
+#define printl(MSG) do { printf(MSG "\n"); } while (0)
+
+#define HANDLE_ERROR(ERR) do { \
+    if (ERR) { \
+        printf("Error: %d\n", ERR); \
+        return 1; \
+    } \
+} while (0)
+
 
 int main() {
-    printf("Hello Measure!");
+    printl("Initiallizing EMA...");
+    int err = EMA_init(NULL);
+    HANDLE_ERROR(err);
+
+    printl("Finalizing EMA...");
+    err = EMA_finalize();
+    HANDLE_ERROR(err);
+
     return 0;
 }

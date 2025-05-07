7b1e96a0324d8c0fa133e51754b8f95608cbc238
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-05-07 12:05:37 +0200

    Add minimalistic help message for invalid invocations of ema_measure utility.

--- utils/measure/measure.c
+++ utils/measure/measure.c
@@ -13,7 +13,18 @@
 } while (0)
 
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printl(
+            "USAGE: ema_measure <CMD>\n\n"
+            "CMD - execution command as a string (e.g. 'sleep 5')"
+        );
+        return EXIT_FAILURE;
+    }
+
+    char* cmd = argv[1];
+    printf("CMD: %s\n", cmd);
+
     printl("Initiallizing EMA...");
     int err = EMA_init(NULL);
     HANDLE_ERROR(err);
@@ -21,7 +32,7 @@ int main() {
     EMA_REGION_DECLARE(region);
     EMA_REGION_DEFINE(&region, "region");
 
-    system("sleep 5");
+    system(cmd);
 
     EMA_REGION_BEGIN(region);
     EMA_REGION_END(region);
